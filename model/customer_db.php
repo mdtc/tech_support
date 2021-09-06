1fc9a10ea886df333950a0ab3a92dc6fd4fadec3
Mario Campos <mdtc1818@gmail.com>
2021-09-06 22:07:09 +1000

    up to date

--- model/customer_db.php
+++ model/customer_db.php
@@ -12,9 +12,8 @@ function get_customers() {
 
 function get_customers_by_last_name($last_name) {
     global $db;
-    $query = 'SELECT * FROM customers where lastName LIKE %:last_name%';
+    $query = "SELECT * FROM customers where lastName LIKE '%$last_name%'";
     $statement = $db->prepare($query);
-    $statement -> bindvalue(':last_name', $last_name);
     $statement -> execute();
     $customers = $statement->fetchAll();
     $statement ->closeCursor();
