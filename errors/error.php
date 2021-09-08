7abf45729ba1152a008ba01e7fb6913c465b8753
Mario Campos <mdtc1818@gmail.com>
2021-09-08 22:45:15 +1000

    bug fixed

--- errors/error.php
+++ errors/error.php
@@ -1,10 +1,9 @@
 <?php include '../view/header.php'; ?>
 <main>
-<div  class="mainMenu">
     <div class="mainMenu">
         <h1>Error</h1>
         <p><?php echo htmlspecialchars($error); ?></p>
     </div>
-</div>
+
 </main>
 <?php include '../view/footer.php'; ?>
